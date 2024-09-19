e0a694efb5717c4d436fb751f689dd2a5f6c1888
DavidTekTek <iolamilekann@gmail.com>
2024-09-20 00:34:41 +0100

    output working now

--- Lesson 110 activities/cube of the cube.py
+++ Lesson 110 activities/cube of the cube.py
@@ -2,15 +2,9 @@
 def cube(num):
     return num*num*num
 
-# define a function that execute the function cube if user enter number divisible by 3
-def by_three(num):
-    if num %3 == 0:
-        return cube(num)
-    else:
-        return False
-    
-    # display result
-    print(by_three(9))
-    print(by_three(5))
+# function to find cube
+def cube(num):
+    return num * num * num
 
-    # no output
+num = 5
+print(f"The cube of {num} is {cube(num)}")
